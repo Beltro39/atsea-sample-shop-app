e7588e7cb51e5489098cbb4f10f83a70a259caa8
beltro39 <sbeltrana@unal.edu.co>
2021-12-12 18:19:57 -0500

    FIREFOX

--- app/react-app/src/components/Header/index.js
+++ app/react-app/src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css'
 
 const Header = () => {
-  const title = 'Welcome to the PRUEBA shop'
+  const title = 'Welcome to the FIREFOX shop'
   const subtitle = ''
   return (
     <div className='headerSection'>
