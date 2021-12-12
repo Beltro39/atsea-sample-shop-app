71ab20e2996eed902a8c14bdb4234402b86325f3
beltro39 <sbeltrana@unal.edu.co>
2021-12-12 17:48:48 -0500

    prueba

--- app/react-app/src/components/Header/index.js
+++ app/react-app/src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css'
 
 const Header = () => {
-  const title = 'Welcome to the ATSEA shop'
+  const title = 'Welcome to the PRUEBA shop'
   const subtitle = ''
   return (
     <div className='headerSection'>
