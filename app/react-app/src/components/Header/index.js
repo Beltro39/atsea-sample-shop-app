7f499ca06804342fcfab9a846525cbaf54ab0973
beltro39 <sbeltrana@unal.edu.co>
2021-12-12 21:55:03 -0500

    Docker

--- app/react-app/src/components/Header/index.js
+++ app/react-app/src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css'
 
 const Header = () => {
-  const title = 'Welcome to the FIREFOX-MS6 shop'
+  const title = 'Welcome to the DOCKER shop'
   const subtitle = ''
   return (
     <div className='headerSection'>
