365bc7b0198379ee151f37c1e3084a3e2f4da8a0
beltro39 <sbeltrana@unal.edu.co>
2021-12-12 19:39:35 -0500

    FIREFOX-MS3

--- app/react-app/src/components/Header/index.js
+++ app/react-app/src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css'
 
 const Header = () => {
-  const title = 'Welcome to the FIREFOX-MS2 shop'
+  const title = 'Welcome to the FIREFOX-MS3 shop'
   const subtitle = ''
   return (
     <div className='headerSection'>
