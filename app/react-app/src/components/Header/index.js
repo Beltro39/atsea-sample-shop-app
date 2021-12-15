d2e7078328f83c7752ceb0ba64a4d25cec46d203
beltro39 <sbeltrana@unal.edu.co>
2021-12-15 17:35:07 -0500

    ATSEA

--- app/react-app/src/components/Header/index.js
+++ app/react-app/src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css'
 
 const Header = () => {
-  const title = 'Welcome to the BUCKET2 shop'
+  const title = 'Welcome to the ATSEA shop'
   const subtitle = ''
   return (
     <div className='headerSection'>
